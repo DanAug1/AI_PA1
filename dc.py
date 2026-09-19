c1215847a121e9f98c3c881998c15265ea3966b7
Dan A <dan.a.augusti@gmail.com>
2026-09-18 21:36:11 -0400

    second commit, implemented base functionality for actions and researching how to solve for path cost and heuristic. Need to scrounge search.py

--- dc.py
+++ dc.py
@@ -21,6 +21,8 @@ for line in gzip.open(dict_file, 'rt'):
     n = float(n)
     dictionary[word] = n
 
+
+
 class DC(search.Problem):
     """DC is a subclass of the AIMA search files's Problem class. Its init
        method takes three arguments: the initial word, goal word, and cost method.
@@ -32,11 +34,21 @@ class DC(search.Problem):
        """
 
     def __init__(self, initial='dog', goal='cat', cost='steps'):
-        #TODO: complete this
-        # set instance attributes ...
-        pass
-        # make sure arguments are legal, raising an error if any are bad.
-        pass
+
+        if not (3 <= len(initial) <= 4) or not (3 <= len(goal) <= 4):
+            raise ValueError("Arg length for initial and goal must be 3 or 4 characters long")
+        if (len(initial) != len(goal)):
+            raise ValueError("Character lengths for initial and goal must be the same")
+
+        self.initial = initial
+        self.goal = goal
+
+        if cost.casefold() not in ["steps", "scrabble", "frequency"]:
+            raise ValueError("Cost method must be 'steps' or 'scrabble' or 'frequency'")
+        else:
+            self.cost = cost.casefold()
+
+
 
     def actions(self, state):
         #TODO: complete this
@@ -47,20 +59,50 @@ class DC(search.Problem):
         should not be the same as the state, i.e., don't replace a
         character with the same character """
 
-        pass
+        stateList = []
+
+
+        #is this supposed to only do one index instead of all possible changes?
+        for i in range(len(state)):
+            for letter in "abcdefghijklmnopqrstuvwxyz":
+                if letter == state[i]:
+                    continue
+
+                newWord = state[:i] + letter + state[i+1:]
+
+                if newWord in dictionary:
+                    stateList.append((i, letter))
+
+        return stateList
+
+
 
     def result(self, state, action):
         #TODO: complete this
         """ takes a state and an action and returns a new state """
-        pass
+
+        #unpack the tuple from actions()
+        index, letter = action
+
+        return state[:inex] + letter + state[index+1:]
+
+
 
     def goal_test(self, state):
         #TODO: complete this
         """ returns True iff state is a goal state for this problem instance """
-        pass
+
+        return state == self.goal
+
+
 
     def path_cost(self, c, state1, action, state2):
         #TODO: complete this
+        #how much has my current change cost me? 1 by 1????
+        #after using chatgpt and asking about the abstractions between the heuristic function
+        #and the path_cost function, whatever cost type given for __init__ will be used as
+        #the controller to decide which math needs to be used to calculate cumulative cost
+        #of each state transition
         """ Returns the cost to get to state2 by applying action in
         state1 given that c is the cost to get up to state1. For the 
         the dc problem, you will have to check what
@@ -68,13 +110,18 @@ class DC(search.Problem):
         i.e., is it steps, scrabble or frequency """
         pass
 
+
+
     def __repr__(self):
         #TODO: complete this
         """" return a suitable string to represent this problem instance """
         pass
 
+
+
     def h(self, node):
         #TODO: complete this
+        #How much do i predict the final cost will be???
         """Heuristic: returns an estimate of the cost to get from the
         state of the node to the goal state. The heuristic's value should
         depend on the Problem's cost parameter, self.cost (i.e., steps, scrabble
